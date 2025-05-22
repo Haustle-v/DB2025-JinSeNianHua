5cb4517aea1ba44ca6c93b8bc7bc9771e82efe76
Redempt-opt <qibosu81@gmail.com>
2025-05-22 22:24:21 +0800

    p1初步完成

--- rmdb/src/record/rm_scan.cpp
+++ rmdb/src/record/rm_scan.cpp
@@ -1,7 +1,7 @@
 /* Copyright (c) 2023 Renmin University of China
 RMDB is licensed under Mulan PSL v2.
-You can use this software according to the terms and conditions of the Mulan PSL v2.
-You may obtain a copy of Mulan PSL v2 at:
+You can use this software according to the terms and conditions of the Mulan PSL
+v2. You may obtain a copy of Mulan PSL v2 at:
         http://license.coscl.org.cn/MulanPSL2
 THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
 EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
@@ -9,6 +9,7 @@ MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
 See the Mulan PSL v2 for more details. */
 
 #include "rm_scan.h"
+
 #include "rm_file_handle.h"
 
 /**
@@ -16,32 +17,79 @@ See the Mulan PSL v2 for more details. */
  * @param file_handle
  */
 RmScan::RmScan(const RmFileHandle *file_handle) : file_handle_(file_handle) {
-    // Todo:
-    // 初始化file_handle和rid（指向第一个存放了记录的位置）
+  // Todo:
+  // 初始化file_handle和rid（指向第一个存放了记录的位置）
 
+  int max_page_num = file_handle_->file_hdr_.num_pages;
+  int max_record_size = file_handle_->file_hdr_.num_records_per_page;
+  PageId tmp_page_id{file_handle_->fd_, INVALID_PAGE_ID};
+
+  for (page_id_t page_no = 1; page_no < max_page_num; ++page_no) {
+    RmPageHandle page_hdl = file_handle_->fetch_page_handle(page_no);
+    tmp_page_id.page_no = page_no;
+    int slot_no = Bitmap::next_bit(1, page_hdl.bitmap, max_record_size, -1);
+    if (slot_no != max_record_size) {
+      rid_.page_no = page_no;
+      rid_.slot_no = slot_no;
+      file_handle_->buffer_pool_manager_->unpin_page(tmp_page_id, false);
+      return;
+    }
+    file_handle_->buffer_pool_manager_->unpin_page(tmp_page_id, false);
+  }
+  //   如果全部遍历完了还没有 就设为无效
+  rid_.page_no = INVALID_PAGE_ID;
+  rid_.slot_no = -1;
 }
 
 /**
  * @brief 找到文件中下一个存放了记录的位置
  */
 void RmScan::next() {
-    // Todo:
-    // 找到文件中下一个存放了记录的非空闲位置，用rid_来指向这个位置
+  // Todo:
+  // 找到文件中下一个存放了记录的非空闲位置，用rid_来指向这个位置
 
+  assert(rid_.page_no != INVALID_PAGE_ID && "rm scan next invalid!");
+
+  int max_page_num = file_handle_->file_hdr_.num_pages;
+  int max_record_size = file_handle_->file_hdr_.num_records_per_page;
+  PageId tmp_page_id{file_handle_->fd_, INVALID_PAGE_ID};
+
+  //   顺序扫描
+  int start_slot_no = rid_.slot_no;
+  for (page_id_t page_no = rid_.page_no; page_no < max_page_num; ++page_no) {
+    RmPageHandle page_hdl = file_handle_->fetch_page_handle(page_no);
+    tmp_page_id.page_no = page_no;
+    int slot_no =
+        Bitmap::next_bit(1, page_hdl.bitmap, max_record_size, start_slot_no);
+
+    if (slot_no == max_record_size) {
+      start_slot_no = -1;
+    } else {
+      // 找到了
+      rid_.page_no = page_no;
+      rid_.slot_no = slot_no;
+      file_handle_->buffer_pool_manager_->unpin_page(tmp_page_id, false);
+      return;
+    }
+
+    file_handle_->buffer_pool_manager_->unpin_page(tmp_page_id, false);
+  }
+
+  //   没找到 设为无效
+  rid_.page_no = INVALID_PAGE_ID;
+  rid_.slot_no = -1;
 }
 
 /**
  * @brief ​ 判断是否到达文件末尾
  */
 bool RmScan::is_end() const {
-    // Todo: 修改返回值
+  // Todo: 修改返回值
 
-    return false;
+  return rid_.page_no == INVALID_PAGE_ID;
 }
 
 /**
  * @brief RmScan内部存放的rid
  */
-Rid RmScan::rid() const {
-    return rid_;
-}
+Rid RmScan::rid() const { return rid_; }
--- rmdb/src/storage/disk_manager.cpp
+++ rmdb/src/storage/disk_manager.cpp
@@ -1,7 +1,7 @@
 /* Copyright (c) 2023 Renmin University of China
 RMDB is licensed under Mulan PSL v2.
-You can use this software according to the terms and conditions of the Mulan PSL v2.
-You may obtain a copy of Mulan PSL v2 at:
+You can use this software according to the terms and conditions of the Mulan PSL
+v2. You may obtain a copy of Mulan PSL v2 at:
         http://license.coscl.org.cn/MulanPSL2
 THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
 EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
@@ -17,7 +17,10 @@ See the Mulan PSL v2 for more details. */
 
 #include "defs.h"
 
-DiskManager::DiskManager() { memset(fd2pageno_, 0, MAX_FD * (sizeof(std::atomic<page_id_t>) / sizeof(char))); }
+DiskManager::DiskManager() {
+  memset(fd2pageno_, 0,
+         MAX_FD * (sizeof(std::atomic<page_id_t>) / sizeof(char)));
+}
 
 /**
  * @description: 将数据写入文件的指定磁盘页面中
@@ -26,12 +29,20 @@ DiskManager::DiskManager() { memset(fd2pageno_, 0, MAX_FD * (sizeof(std::atomic<
  * @param {char} *offset 要写入磁盘的数据
  * @param {int} num_bytes 要写入磁盘的数据大小
  */
-void DiskManager::write_page(int fd, page_id_t page_no, const char *offset, int num_bytes) {
-    // Todo:
-    // 1.lseek()定位到文件头，通过(fd,page_no)可以定位指定页面及其在磁盘文件中的偏移量
-    // 2.调用write()函数
-    // 注意write返回值与num_bytes不等时 throw InternalError("DiskManager::write_page Error");
-
+void DiskManager::write_page(int fd, page_id_t page_no, const char *offset,
+                             int num_bytes) {
+  // Todo:
+  // 1.lseek()定位到文件头，通过(fd,page_no)可以定位指定页面及其在磁盘文件中的偏移量
+  // 2.调用write()函数
+  // 注意write返回值与num_bytes不等时 throw
+  // InternalError("DiskManager::write_page Error");
+  std::scoped_lock<std::mutex> lock(latch_);
+  lseek(fd, page_no * PAGE_SIZE, SEEK_SET);
+  ssize_t write_bytes = write(fd, offset, num_bytes);
+  if (write_bytes != num_bytes) {
+    throw InternalError("DiskManager::write_page Error" +
+                        std::string(strerror(errno)));
+  }
 }
 
 /**
@@ -41,12 +52,20 @@ void DiskManager::write_page(int fd, page_id_t page_no, const char *offset, int 
  * @param {char} *offset 读取的内容写入到offset中
  * @param {int} num_bytes 读取的数据量大小
  */
-void DiskManager::read_page(int fd, page_id_t page_no, char *offset, int num_bytes) {
-    // Todo:
-    // 1.lseek()定位到文件头，通过(fd,page_no)可以定位指定页面及其在磁盘文件中的偏移量
-    // 2.调用read()函数
-    // 注意read返回值与num_bytes不等时，throw InternalError("DiskManager::read_page Error");
-
+void DiskManager::read_page(int fd, page_id_t page_no, char *offset,
+                            int num_bytes) {
+  // Todo:
+  // 1.lseek()定位到文件头，通过(fd,page_no)可以定位指定页面及其在磁盘文件中的偏移量
+  // 2.调用read()函数
+  // 注意read返回值与num_bytes不等时，throw
+  // InternalError("DiskManager::read_page Error");
+  std::scoped_lock<std::mutex> lock(latch_);
+  lseek(fd, page_no * PAGE_SIZE, SEEK_SET);
+  ssize_t read_bytes = read(fd, offset, num_bytes);
+  if (read_bytes != num_bytes) {
+    throw InternalError("DiskManager::read_page Error" +
+                        std::string(strerror(errno)));
+  }
 }
 
 /**
@@ -55,42 +74,42 @@ void DiskManager::read_page(int fd, page_id_t page_no, char *offset, int num_byt
  * @param {int} fd 指定文件的文件句柄
  */
 page_id_t DiskManager::allocate_page(int fd) {
-    // 简单的自增分配策略，指定文件的页面编号加1
-    assert(fd >= 0 && fd < MAX_FD);
-    return fd2pageno_[fd]++;
+  // 简单的自增分配策略，指定文件的页面编号加1
+  assert(fd >= 0 && fd < MAX_FD);
+  return fd2pageno_[fd]++;
 }
 
 void DiskManager::deallocate_page(__attribute__((unused)) page_id_t page_id) {}
 
-bool DiskManager::is_dir(const std::string& path) {
-    struct stat st;
-    return stat(path.c_str(), &st) == 0 && S_ISDIR(st.st_mode);
+bool DiskManager::is_dir(const std::string &path) {
+  struct stat st;
+  return stat(path.c_str(), &st) == 0 && S_ISDIR(st.st_mode);
 }
 
 void DiskManager::create_dir(const std::string &path) {
-    // Create a subdirectory
-    std::string cmd = "mkdir " + path;
-    if (system(cmd.c_str()) < 0) {  // 创建一个名为path的目录
-        throw UnixError();
-    }
+  // Create a subdirectory
+  std::string cmd = "mkdir " + path;
+  if (system(cmd.c_str()) < 0) {  // 创建一个名为path的目录
+    throw UnixError();
+  }
 }
 
 void DiskManager::destroy_dir(const std::string &path) {
-    std::string cmd = "rm -r " + path;
-    if (system(cmd.c_str()) < 0) {
-        throw UnixError();
-    }
+  std::string cmd = "rm -r " + path;
+  if (system(cmd.c_str()) < 0) {
+    throw UnixError();
+  }
 }
 
 /**
  * @description: 判断指定路径文件是否存在
- * @return {bool} 若指定路径文件存在则返回true 
+ * @return {bool} 若指定路径文件存在则返回true
  * @param {string} &path 指定路径文件
  */
 bool DiskManager::is_file(const std::string &path) {
-    // 用struct stat获取文件信息
-    struct stat st;
-    return stat(path.c_str(), &st) == 0 && S_ISREG(st.st_mode);
+  // 用struct stat获取文件信息
+  struct stat st;
+  return stat(path.c_str(), &st) == 0 && S_ISREG(st.st_mode);
 }
 
 /**
@@ -99,9 +118,19 @@ bool DiskManager::is_file(const std::string &path) {
  * @param {string} &path
  */
 void DiskManager::create_file(const std::string &path) {
-    // Todo:
-    // 调用open()函数，使用O_CREAT模式
-    // 注意不能重复创建相同文件
+  // Todo:
+  // 调用open()函数，使用O_CREAT模式
+  // 注意不能重复创建相同文件
+
+  if (is_file(path)) {
+    throw FileExistsError(path);
+  }
+  int fd = open(path.c_str(), O_CREAT, 0666);
+  if (fd == -1) {
+    // 感觉unix error应该更合适一点 但主打一个方便找错
+    throw InternalError("DiskManager create file failed");
+  }
+  close(fd);
 }
 
 /**
@@ -109,36 +138,75 @@ void DiskManager::create_file(const std::string &path) {
  * @param {string} &path 文件所在路径
  */
 void DiskManager::destroy_file(const std::string &path) {
-    // Todo:
-    // 调用unlink()函数
-    // 注意不能删除未关闭的文件
-    
+  // Todo:
+  // 调用unlink()函数
+  // 注意不能删除未关闭的文件
+
+  //   暂时不确定是否报错
+  if (!is_file(path)) {
+    throw FileNotFoundError(path);
+  }
+  auto iter = path2fd_.find(path);
+  if (iter != path2fd_.end()) {
+    throw FileNotClosedError(path);
+  }
+  int result = unlink(path.c_str());
+  if (result == -1) {
+    // 删除失败的情况
+    throw InternalError("DiskManager destory file failed" +
+                        std::string(strerror(errno)));
+  }
 }
 
-
 /**
- * @description: 打开指定路径文件 
+ * @description: 打开指定路径文件
  * @return {int} 返回打开的文件的文件句柄
  * @param {string} &path 文件所在路径
  */
 int DiskManager::open_file(const std::string &path) {
-    // Todo:
-    // 调用open()函数，使用O_RDWR模式
-    // 注意不能重复打开相同文件，并且需要更新文件打开列表
-
+  // Todo:
+  // 调用open()函数，使用O_RDWR模式
+  // 注意不能重复打开相同文件，并且需要更新文件打开列表
+  std::scoped_lock<std::mutex> lock(latch_);
+  if (!is_file(path)) {
+    throw FileNotFoundError(path);
+  }
+  auto iter = path2fd_.find(path);
+  //   文件已在内存
+  if (iter != path2fd_.end()) {
+    throw FileNotClosedError(path);
+  }
+  int fd = open(path.c_str(), O_RDWR);
+  if (fd == -1) {
+    throw InternalError("DiskManager open file failed " +
+                        std::string(strerror(errno)));
+  }
+  path2fd_.emplace(path, fd);
+  fd2path_.emplace(fd, path);
+  return fd;
 }
 
 /**
- * @description:用于关闭指定路径文件 
+ * @description:用于关闭指定路径文件
  * @param {int} fd 打开的文件的文件句柄
  */
 void DiskManager::close_file(int fd) {
-    // Todo:
-    // 调用close()函数
-    // 注意不能关闭未打开的文件，并且需要更新文件打开列表
-
+  // Todo:
+  // 调用close()函数
+  // 注意不能关闭未打开的文件，并且需要更新文件打开列表
+  std::scoped_lock<std::mutex> lock(latch_);
+  auto iter = fd2path_.find(fd);
+  if (iter == fd2path_.end()) {
+    throw FileNotOpenError(fd);
+  }
+  int result = close(fd);
+  if (result == -1) {
+    throw InternalError("DiskManager close file failed" +
+                        std::string(strerror(errno)));
+  }
+  path2fd_.erase(fd2path_[fd]);
+  fd2path_.erase(fd);
 }
-
 
 /**
  * @description: 获得文件的大小
@@ -146,9 +214,9 @@ void DiskManager::close_file(int fd) {
  * @param {string} &file_name 文件名
  */
 int DiskManager::get_file_size(const std::string &file_name) {
-    struct stat stat_buf;
-    int rc = stat(file_name.c_str(), &stat_buf);
-    return rc == 0 ? stat_buf.st_size : -1;
+  struct stat stat_buf;
+  int rc = stat(file_name.c_str(), &stat_buf);
+  return rc == 0 ? stat_buf.st_size : -1;
 }
 
 /**
@@ -157,10 +225,10 @@ int DiskManager::get_file_size(const std::string &file_name) {
  * @param {int} fd 文件句柄
  */
 std::string DiskManager::get_file_name(int fd) {
-    if (!fd2path_.count(fd)) {
-        throw FileNotOpenError(fd);
-    }
-    return fd2path_[fd];
+  if (!fd2path_.count(fd)) {
+    throw FileNotOpenError(fd);
+  }
+  return fd2path_[fd];
 }
 
 /**
@@ -169,12 +237,11 @@ std::string DiskManager::get_file_name(int fd) {
  * @param {string} &file_name 文件名
  */
 int DiskManager::get_file_fd(const std::string &file_name) {
-    if (!path2fd_.count(file_name)) {
-        return open_file(file_name);
-    }
-    return path2fd_[file_name];
+  if (!path2fd_.count(file_name)) {
+    return open_file(file_name);
+  }
+  return path2fd_[file_name];
 }
-
 
 /**
  * @description:  读取日志文件内容
@@ -184,23 +251,22 @@ int DiskManager::get_file_fd(const std::string &file_name) {
  * @param {int} offset 读取的内容在文件中的位置
  */
 int DiskManager::read_log(char *log_data, int size, int offset) {
-    // read log file from the previous end
-    if (log_fd_ == -1) {
-        log_fd_ = open_file(LOG_FILE_NAME);
-    }
-    int file_size = get_file_size(LOG_FILE_NAME);
-    if (offset > file_size) {
-        return -1;
-    }
+  // read log file from the previous end
+  if (log_fd_ == -1) {
+    log_fd_ = open_file(LOG_FILE_NAME);
+  }
+  int file_size = get_file_size(LOG_FILE_NAME);
+  if (offset > file_size) {
+    return -1;
+  }
 
-    size = std::min(size, file_size - offset);
-    if(size == 0) return 0;
-    lseek(log_fd_, offset, SEEK_SET);
-    ssize_t bytes_read = read(log_fd_, log_data, size);
-    assert(bytes_read == size);
-    return bytes_read;
+  size = std::min(size, file_size - offset);
+  if (size == 0) return 0;
+  lseek(log_fd_, offset, SEEK_SET);
+  ssize_t bytes_read = read(log_fd_, log_data, size);
+  assert(bytes_read == size);
+  return bytes_read;
 }
-
 
 /**
  * @description: 写日志内容
@@ -208,14 +274,14 @@ int DiskManager::read_log(char *log_data, int size, int offset) {
  * @param {int} size 要写入的内容大小
  */
 void DiskManager::write_log(char *log_data, int size) {
-    if (log_fd_ == -1) {
-        log_fd_ = open_file(LOG_FILE_NAME);
-    }
+  if (log_fd_ == -1) {
+    log_fd_ = open_file(LOG_FILE_NAME);
+  }
 
-    // write from the file_end
-    lseek(log_fd_, 0, SEEK_END);
-    ssize_t bytes_write = write(log_fd_, log_data, size);
-    if (bytes_write != size) {
-        throw UnixError();
-    }
+  // write from the file_end
+  lseek(log_fd_, 0, SEEK_END);
+  ssize_t bytes_write = write(log_fd_, log_data, size);
+  if (bytes_write != size) {
+    throw UnixError();
+  }
 }
