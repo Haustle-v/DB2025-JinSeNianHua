9303ebd1b0b165ea2729a70fa3cf11ebec3be7a2
Redempt-opt <qibosu81@gmail.com>
2025-06-01 20:35:34 +0800

    create index 修复尝试2

--- src/system/sm_manager.cpp
+++ src/system/sm_manager.cpp
@@ -281,8 +281,8 @@ void SmManager::create_index(const std::string &tab_name, const std::vector<std:
   //   创建索引 插入记录
   ix_manager_->create_index(tab_name, index.cols);
   auto ix_hdl_ptr = ix_manager_->open_index(tab_name, col_names);
-  // auto file_hdl_ptr = fhs_[tab_name].get();
-  char key_buffer[col_len];
+  auto file_hdl_ptr = fhs_[tab_name].get();
+  // char key_buffer[col_len];
   // char *key_buffer = new char[col_len];
   //   for (RmScan scan(file_hdl_ptr); !scan.is_end(); scan.next()) {
   //     auto rec_ptr = file_hdl_ptr->get_record(scan.rid(), context);
