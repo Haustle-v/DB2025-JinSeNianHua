449ea985ac205c0ebfaa14957ab93fd349f9a2da
haustle <2765797576@qq.com>
2025-06-05 17:57:55 +0800

    p6试着commit一个文件

--- src/parser/yacc.tab.hpp
+++ src/parser/yacc.tab.hpp
@@ -85,14 +85,23 @@ extern int yydebug;
     TXN_ABORT = 286,               /* TXN_ABORT  */
     TXN_ROLLBACK = 287,            /* TXN_ROLLBACK  */
     ORDER_BY = 288,                /* ORDER_BY  */
-    LEQ = 289,                     /* LEQ  */
-    NEQ = 290,                     /* NEQ  */
-    GEQ = 291,                     /* GEQ  */
-    T_EOF = 292,                   /* T_EOF  */
-    IDENTIFIER = 293,              /* IDENTIFIER  */
-    VALUE_STRING = 294,            /* VALUE_STRING  */
-    VALUE_INT = 295,               /* VALUE_INT  */
-    VALUE_FLOAT = 296              /* VALUE_FLOAT  */
+    ENABLE_NESTLOOP = 289,         /* ENABLE_NESTLOOP  */
+    ENABLE_SORTMERGE = 290,        /* ENABLE_SORTMERGE  */
+    INNER = 291,                   /* INNER  */
+    LEFT = 292,                    /* LEFT  */
+    RIGHT = 293,                   /* RIGHT  */
+    FULL = 294,                    /* FULL  */
+    SEMI = 295,                    /* SEMI  */
+    ON = 296,                      /* ON  */
+    LEQ = 297,                     /* LEQ  */
+    NEQ = 298,                     /* NEQ  */
+    GEQ = 299,                     /* GEQ  */
+    T_EOF = 300,                   /* T_EOF  */
+    IDENTIFIER = 301,              /* IDENTIFIER  */
+    VALUE_STRING = 302,            /* VALUE_STRING  */
+    VALUE_INT = 303,               /* VALUE_INT  */
+    VALUE_FLOAT = 304,             /* VALUE_FLOAT  */
+    VALUE_BOOL = 305               /* VALUE_BOOL  */
   };
   typedef enum yytokentype yytoken_kind_t;
 #endif
