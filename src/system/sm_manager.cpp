259c0798af31237d8d2b6e20de1fd237a2b38db4
Redempt-opt <qibosu81@gmail.com>
2025-06-16 11:01:05 +0800

    修复drop table的bug

--- src/system/sm_manager.cpp
+++ src/system/sm_manager.cpp
@@ -24,7 +24,7 @@ See the Mulan PSL v2 for more details. */
  * @return {bool} 返回是否为一个文件夹
  * @param {string&} db_name 数据库文件名称，与文件夹同名
  */
-bool SmManager::is_dir(const std::string& db_name) {
+bool SmManager::is_dir(const std::string &db_name) {
   struct stat st;
   return stat(db_name.c_str(), &st) == 0 && S_ISDIR(st.st_mode);
 }
@@ -33,7 +33,7 @@ bool SmManager::is_dir(const std::string& db_name) {
  * @description: 创建数据库，所有的数据库相关文件都放在数据库同名文件夹下
  * @param {string&} db_name 数据库名称
  */
-void SmManager::create_db(const std::string& db_name) {
+void SmManager::create_db(const std::string &db_name) {
   if (is_dir(db_name)) {
     throw DatabaseExistsError(db_name);
   }
@@ -46,7 +46,7 @@ void SmManager::create_db(const std::string& db_name) {
     throw UnixError();
   }
   // 创建系统目录
-  DbMeta* new_db = new DbMeta();
+  DbMeta *new_db = new DbMeta();
   new_db->name_ = db_name;
 
   // 注意，此处ofstream会在当前目录创建(如果没有此文件先创建)和打开一个名为DB_META_NAME的文件
@@ -70,7 +70,7 @@ void SmManager::create_db(const std::string& db_name) {
  * @description: 删除数据库，同时需要清空相关文件以及数据库同名文件夹
  * @param {string&} db_name 数据库名称，与文件夹同名
  */
-void SmManager::drop_db(const std::string& db_name) {
+void SmManager::drop_db(const std::string &db_name) {
   if (!is_dir(db_name)) {
     throw DatabaseNotFoundError(db_name);
   }
@@ -85,7 +85,7 @@ void SmManager::drop_db(const std::string& db_name) {
  * 打开数据库，找到数据库对应的文件夹，并加载数据库元数据和相关文件
  * @param {string&} db_name 数据库名称，与文件夹同名
  */
-void SmManager::open_db(const std::string& db_name) {
+void SmManager::open_db(const std::string &db_name) {
   // sqb :完成数据部分 未考虑索引 5.22
   // 找到目录 并进入
   if (!is_dir(db_name)) {
@@ -100,7 +100,7 @@ void SmManager::open_db(const std::string& db_name) {
   ifs >> db_;
 
   //   加载数据文件
-  for (auto& entry : db_.tabs_) {
+  for (auto &entry : db_.tabs_) {
     fhs_.emplace(entry.first, rm_manager_->open_file(entry.first));
     // 加载表的索引文件 留到第三问
     // for(auto &)
@@ -124,7 +124,7 @@ void SmManager::close_db() {
   // 更新元数据
   flush_meta();
   // 更新数据
-  for (auto& entry : fhs_) {
+  for (auto &entry : fhs_) {
     rm_manager_->close_file(entry.second.get());
   }
   // 更新索引
@@ -136,7 +136,7 @@ void SmManager::close_db() {
  * 显示所有的表,通过测试需要将其结果写入到output.txt,详情看题目文档
  * @param {Context*} context
  */
-void SmManager::show_tables(Context* context) {
+void SmManager::show_tables(Context *context) {
   std::fstream outfile;
   outfile.open("output.txt", std::ios::out | std::ios::app);
   outfile << "| Tables |\n";
@@ -144,8 +144,8 @@ void SmManager::show_tables(Context* context) {
   printer.print_separator(context);
   printer.print_record({"Tables"}, context);
   printer.print_separator(context);
-  for (auto& entry : db_.tabs_) {
-    auto& tab = entry.second;
+  for (auto &entry : db_.tabs_) {
+    auto &tab = entry.second;
     printer.print_record({tab.name}, context);
     outfile << "| " << tab.name << " |\n";
   }
@@ -158,8 +158,8 @@ void SmManager::show_tables(Context* context) {
  * @param {string&} tab_name 表名称
  * @param {Context*} context
  */
-void SmManager::desc_table(const std::string& tab_name, Context* context) {
-  TabMeta& tab = db_.get_table(tab_name);
+void SmManager::desc_table(const std::string &tab_name, Context *context) {
+  TabMeta &tab = db_.get_table(tab_name);
 
   std::vector<std::string> captions = {"Field", "Type", "Index"};
   RecordPrinter printer(captions.size());
@@ -168,9 +168,8 @@ void SmManager::desc_table(const std::string& tab_name, Context* context) {
   printer.print_record(captions, context);
   printer.print_separator(context);
   // Print fields
-  for (auto& col : tab.cols) {
-    std::vector<std::string> field_info = {col.name, coltype2str(col.type),
-                                           col.index ? "YES" : "NO"};
+  for (auto &col : tab.cols) {
+    std::vector<std::string> field_info = {col.name, coltype2str(col.type), col.index ? "YES" : "NO"};
     printer.print_record(field_info, context);
   }
   // Print footer
@@ -183,9 +182,7 @@ void SmManager::desc_table(const std::string& tab_name, Context* context) {
  * @param {vector<ColDef>&} col_defs 表的字段
  * @param {Context*} context
  */
-void SmManager::create_table(const std::string& tab_name,
-                             const std::vector<ColDef>& col_defs,
-                             Context* context) {
+void SmManager::create_table(const std::string &tab_name, const std::vector<ColDef> &col_defs, Context *context) {
   if (db_.is_table(tab_name)) {
     throw TableExistsError(tab_name);
   }
@@ -193,7 +190,7 @@ void SmManager::create_table(const std::string& tab_name,
   int curr_offset = 0;
   TabMeta tab;
   tab.name = tab_name;
-  for (auto& col_def : col_defs) {
+  for (auto &col_def : col_defs) {
     ColMeta col = {.tab_name = tab_name,
                    .name = col_def.name,
                    .type = col_def.type,
@@ -204,9 +201,8 @@ void SmManager::create_table(const std::string& tab_name,
     tab.cols.push_back(col);
   }
   // Create & open record file
-  int record_size =
-      curr_offset;  // record_size就是col
-                    // meta所占的大小（表的元数据也是以记录的形式进行存储的）
+  int record_size = curr_offset;  // record_size就是col
+                                  // meta所占的大小（表的元数据也是以记录的形式进行存储的）
   rm_manager_->create_file(tab_name, record_size);
   db_.tabs_[tab_name] = tab;
   // fhs_[tab_name] = rm_manager_->open_file(tab_name);
@@ -220,21 +216,32 @@ void SmManager::create_table(const std::string& tab_name,
  * @param {string&} tab_name 表的名称
  * @param {Context*} context
  */
-void SmManager::drop_table(const std::string& tab_name, Context* context) {
-  // sqb :完成数据部分 未考虑索引 5.22
+void SmManager::drop_table(const std::string &tab_name, Context *context) {
+  // sqb :完成数据与索引 5.29
+
   if (!db_.is_table(tab_name)) {
     throw TableNotFoundError(tab_name);
   }
 
-  //   删除内存中相关表元数据
-  db_.tabs_.erase(tab_name);
-  flush_meta();
+  // sqb 删表时删除缓冲池的内容 与dropindex类似，缓冲池从1开始 6.16
+  RmFileHandle *rm_hdl_ptr = fhs_[tab_name].get();
+  int record_page_num = rm_hdl_ptr->get_page_num();
+  for (page_id_t page_no = 1; page_no < record_page_num; ++page_no) {
+    buffer_pool_manager_->delete_page({rm_hdl_ptr->GetFd(), page_no});
+  }
 
+  //   删除内存中相关表元数据
   rm_manager_->close_file(fhs_[tab_name].get());
   rm_manager_->destroy_file(tab_name);
-  fhs_.erase(tab_name);
 
-  //   ihs_.erase(tab_name); //还有索引
+  TabMeta &tab_meta = db_.tabs_[tab_name];
+  for (auto &index_meta : tab_meta.indexes) {
+    drop_index(tab_name, index_meta.cols, context);
+  }
+
+  fhs_.erase(tab_name);
+  db_.tabs_.erase(tab_name);
+  flush_meta();
 }
 
 /**
@@ -243,9 +250,8 @@ void SmManager::drop_table(const std::string& tab_name, Context* context) {
  * @param {vector<string>&} col_names 索引包含的字段名称
  * @param {Context*} context
  */
-void SmManager::create_index(const std::string& tab_name,
-                             const std::vector<std::string>& col_names,
-                             Context* context) {}
+void SmManager::create_index(const std::string &tab_name, const std::vector<std::string> &col_names, Context *context) {
+}
 
 /**
  * @description: 删除索引
@@ -253,9 +259,7 @@ void SmManager::create_index(const std::string& tab_name,
  * @param {vector<string>&} col_names 索引包含的字段名称
  * @param {Context*} context
  */
-void SmManager::drop_index(const std::string& tab_name,
-                           const std::vector<std::string>& col_names,
-                           Context* context) {}
+void SmManager::drop_index(const std::string &tab_name, const std::vector<std::string> &col_names, Context *context) {}
 
 /**
  * @description: 删除索引
@@ -263,7 +267,4 @@ void SmManager::drop_index(const std::string& tab_name,
  * @param {vector<ColMeta>&} 索引包含的字段元数据
  * @param {Context*} context
  */
-void SmManager::drop_index(const std::string& tab_name,
-                           const std::vector<ColMeta>& cols, Context* context) {
-
-}
+void SmManager::drop_index(const std::string &tab_name, const std::vector<ColMeta> &cols, Context *context) {}
