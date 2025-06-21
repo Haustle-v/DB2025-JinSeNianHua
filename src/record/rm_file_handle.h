431ca2fa9fb278a62d0b1ef8885151e74b3b3dd2
Redempt-opt <qibosu81@gmail.com>
2025-06-21 21:22:15 +0800

    修复merge的问题

--- src/record/rm_file_handle.h
+++ src/record/rm_file_handle.h
@@ -27,29 +27,39 @@ struct TabMeta;  // sqb
 struct RmPageHandle {
   const RmFileHdr *file_hdr;  // 当前页面所在文件的文件头指针
   Page *page;                 // 页面的实际数据，包括页面存储的数据、元信息等
-  RmPageHdr *page_hdr;        // page->data的第一部分，存储页面元信息，指针指向首地址，长度为sizeof(RmPageHdr)
-  char *bitmap;               // page->data的第二部分，存储页面的bitmap，指针指向首地址，长度为file_hdr->bitmap_size
-  char *slots;  // page->data的第三部分，存储表的记录，指针指向首地址，每个slot的长度为file_hdr->record_size
+  RmPageHdr *
+      page_hdr;  // page->data的第一部分，存储页面元信息，指针指向首地址，长度为sizeof(RmPageHdr)
+  char *
+      bitmap;  // page->data的第二部分，存储页面的bitmap，指针指向首地址，长度为file_hdr->bitmap_size
+  char *
+      slots;  // page->data的第三部分，存储表的记录，指针指向首地址，每个slot的长度为file_hdr->record_size
 
-  RmPageHandle(const RmFileHdr *fhdr_, Page *page_) : file_hdr(fhdr_), page(page_) {
-    page_hdr = reinterpret_cast<RmPageHdr *>(page->get_data() + page->OFFSET_PAGE_HDR);
+  RmPageHandle(const RmFileHdr *fhdr_, Page *page_)
+      : file_hdr(fhdr_), page(page_) {
+    page_hdr =
+        reinterpret_cast<RmPageHdr *>(page->get_data() + page->OFFSET_PAGE_HDR);
     bitmap = page->get_data() + sizeof(RmPageHdr) + page->OFFSET_PAGE_HDR;
     slots = bitmap + file_hdr->bitmap_size;
   }
 
   //   // 返回指定slot_no的slot存储收地址
   //   char *get_slot(int slot_no) const {
-  //     return slots + slot_no * file_hdr->record_size;  // slots的首地址 + slot个数 *
-  //                                                      // 每个slot的大小(每个record的大小)
+  //     return slots + slot_no * file_hdr->record_size;  // slots的首地址 +
+  //     slot个数 *
+  //                                                      //
+  //                                                      每个slot的大小(每个record的大小)
   //   }
 
   // sqb 重要改动 为所有record头部添加了tuple meta 6.17
   char *get_slot_record(int slot_no) const {
-    return slots + slot_no * (file_hdr->record_size + sizeof(TupleMeta)) + sizeof(TupleMeta);
+    return slots + slot_no * (file_hdr->record_size + sizeof(TupleMeta)) +
+           sizeof(TupleMeta);
   }
 
   // sqb 重要改动 为所有record头部添加了tuple meta 6.17
-  char *get_slot_meta(int slot_no) const { return slots + slot_no * (file_hdr->record_size + sizeof(TupleMeta)); }
+  char *get_slot_meta(int slot_no) const {
+    return slots + slot_no * (file_hdr->record_size + sizeof(TupleMeta));
+  }
 };
 
 /* 每个RmFileHandle对应一个表的数据文件，里面有多个page，每个page的数据封装在RmPageHandle中
@@ -67,12 +77,16 @@ class RmFileHandle {
   mutable std::shared_mutex latch_;  // sqb 加锁保证线程安全 6.17
 
  public:
-  RmFileHandle(DiskManager *disk_manager, BufferPoolManager *buffer_pool_manager, int fd)
-      : disk_manager_(disk_manager), buffer_pool_manager_(buffer_pool_manager), fd_(fd) {
+  RmFileHandle(DiskManager *disk_manager,
+               BufferPoolManager *buffer_pool_manager, int fd)
+      : disk_manager_(disk_manager),
+        buffer_pool_manager_(buffer_pool_manager),
+        fd_(fd) {
     // 注意：这里从磁盘中读出文件描述符为fd的文件的file_hdr，读到内存中
     // 这里实际就是初始化file_hdr，只不过是从磁盘中读出进行初始化
     // init file_hdr_
-    disk_manager_->read_page(fd, RM_FILE_HDR_PAGE, (char *)&file_hdr_, sizeof(file_hdr_));
+    disk_manager_->read_page(fd, RM_FILE_HDR_PAGE, (char *)&file_hdr_,
+                             sizeof(file_hdr_));
     // disk_manager管理的fd对应的文件中，设置从file_hdr_.num_pages开始分配page_no
     disk_manager_->set_fd2pageno(fd, file_hdr_.num_pages);
   }
@@ -90,14 +104,18 @@ class RmFileHandle {
   std::unique_ptr<RmRecord> get_record(const Rid &rid, Context *context) const;
 
   // sqb 再次修改增删改接口 让undo link同时更新
-  Rid insert_record(char *buf, Context *context, const TabMeta *schema = nullptr);
+  Rid insert_record(char *buf, Context *context,
+                    const TabMeta *schema = nullptr);
 
   void insert_record(const Rid &rid, char *buf);
 
   // sqb 6.4更改 delete update 接口 便于封装事务与日志
-  void delete_record(const Rid &rid, Context *context, RmRecord *old_rec = nullptr, const TabMeta *schema = nullptr);
+  void delete_record(const Rid &rid, Context *context,
+                     RmRecord *old_rec = nullptr,
+                     const TabMeta *schema = nullptr);
 
-  void update_record(const Rid &rid, char *buf, Context *context, RmRecord *old_rec = nullptr,
+  void update_record(const Rid &rid, char *buf, Context *context,
+                     RmRecord *old_rec = nullptr,
                      const TabMeta *schema = nullptr);
 
   RmPageHandle create_new_page_handle();
@@ -115,7 +133,8 @@ class RmFileHandle {
       -> std::tuple<TupleMeta, RmRecord, std::optional<UndoLink>>;
 
   // sqb 6.20 获取对应版本的tuple
-  auto get_reconstructed_tuple(const Rid &rid, Context *context, TabMeta &tab) -> std::unique_ptr<RmRecord>;
+  auto get_reconstructed_tuple(const Rid &rid, Context *context, TabMeta &tab)
+      -> std::unique_ptr<RmRecord>;
 
   // sqb 事务提交更新所有时间戳 事务回滚时用来
   void set_meta(const Rid &rid, timestamp_t ts, bool is_delete);
