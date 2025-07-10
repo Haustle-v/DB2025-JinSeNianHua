a7f2958397120cb6665030081b19c0f00047fabe
Redempt-opt <qibosu81@gmail.com>
2025-07-10 15:11:27 +0800

    将load的记录同时刷盘

--- src/rmdb.cpp
+++ src/rmdb.cpp
@@ -147,8 +147,10 @@ void *client_handler(void *sock_fd) {
       std::string csv_file = load_stmt.substr(5, csv_file_end - 5);
       std::string tab_name = load_stmt.substr(tab_name_start, tab_name_end - tab_name_start);
 
-      futures.emplace_back(
-          std::async(std::launch::async, [csv_file, tab_name] { sm_manager->load_csv_data(csv_file, tab_name); }));
+      futures.emplace_back(std::async(std::launch::async, [csv_file, tab_name] {
+        sm_manager->load_csv_data(csv_file, tab_name);
+        buffer_pool_manager->flush_all_pages(sm_manager->fhs_.at(tab_name)->GetFd());
+      }));
       //   sm_manager->load_csv_data(csv_file, tab_name);
       if (write(fd, data_send, offset + 1) == -1) {
         break;
